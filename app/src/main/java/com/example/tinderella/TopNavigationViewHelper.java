5796642a6bc568961e7634ee77bfed08dec04352
Paulina <paulina.jalosinska@onet.pl>
2021-06-30 16:45:14 +0300

    część 9

---
+++ app/src/main/java/com/example/tinderella/TopNavigationViewHelper.java
@@ -0,0 +1,40 @@
+package com.example.tinderella;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.core.Context;
+
+public class TopNavigationViewHelper {
+    private static final String TAG = "TopNAvigationViewHelper";
+    public static void setupTopNavigationView(BottomNavigationView tv) {
+        Log.d(TAG, "setupTopNavigationView: stting up navigationview");
+    }
+    public  static void enableNavigation(final Context context, BottomNavigationView view){
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ic_profile:
+                        Intent i = new Intent(context, SettingsActivity.class);
+                        context.startActivity(i);
+                        break;
+                    case R.id.ic_matched:
+                        Intent i1 = new Intent(context, MatchesActivity.class);
+                        context.startActivity(i1);
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+
+
+    }
+}
